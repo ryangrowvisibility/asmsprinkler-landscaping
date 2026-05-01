1165d34208bd732ca56790d004e8ad8a504ea41d
Ryan Irwin <rcirwin11@gmail.com>
2026-05-01 10:11:12 -0400

    fix: add embedded Google Maps iframe to contact section

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -117,6 +117,21 @@ export default function Contact() {
             </aside>
           </FadeUp>
         </div>
+
+        <FadeUp delay={150} className="mt-14">
+          <div className="rounded-[1.75rem] overflow-hidden ring-1 ring-[var(--hairline)] h-96">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3169.5287546483697!2d-119.22738!3d36.32829!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8099b4c9c9c9c9c9%3A0x0!2s2726%20W%20Whitendale%20Ave%2C%20Visalia%2C%20CA%2093277!5e0!3m2!1sen!2sus!4v1234567890"
+              width="100%"
+              height="100%"
+              style={{ border: 0 }}
+              allowFullScreen
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
+        </FadeUp>
+        </div>
       </div>
     </section>
   );
